c03224c3c364e3d45acae67ece09ef503e1f3a0c
Marcos Portela <marcosnasp@gmail.com>
2020-02-17 14:11:28 -0300

    Commit Inicial - gs-actuator-service

---
+++ gs-actuator-service/src/main/java/com/example/gsactuatorservice/GsActuatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsactuatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsActuatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsActuatorServiceApplication.class, args);
+	}
+
+}
---
+++ gs-actuator-service/src/test/java/com/example/gsactuatorservice/GsActuatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsactuatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsActuatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
